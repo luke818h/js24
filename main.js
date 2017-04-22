bce5224e4ae42888ca84b8f587a189a08aaae97e
Luke Henwood <luke8h@gmail.com>
2017-04-22 12:48:54 -0500

    Find white space and more testing in expression

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// Setup
+var testString = "How many q q q q q q q spaces are there in this sentence?";
+
+// Only change code below this line.
+
+var expression = /\s+/g;  // Change this line
+
+// Only change code above this line
+
+// This code counts the matches of expression in testString
+var spaceCount = testString.match(expression).length;
+console.log(spaceCount);
